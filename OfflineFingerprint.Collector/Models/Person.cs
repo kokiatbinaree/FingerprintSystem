351d809f258a5e74df860fe5c4a71f6919dee407
kokiatbinaree <kokiatbinaree@gmail.com>
2026-08-21 09:35:00 +0700

    Complete Person model

--- OfflineFingerprint.Collector/Models/Person.cs
+++ OfflineFingerprint.Collector/Models/Person.cs
@@ -6,6 +6,8 @@ public class Person
     public string PersonCode { get; set; } = "";
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
+    public string? NationalId { get; set; }
+    public string? Note { get; set; }
     public DateTime CreatedAtUtc { get; set; }
     public DateTime UpdatedAtUtc { get; set; }
     public List<FingerprintImage> FingerprintImages { get; set; } = [];
