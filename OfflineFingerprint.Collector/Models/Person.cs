e3b569d074d73090399234971e66c643cc602469
kokiatbinaree <kokiatbinaree@gmail.com>
2026-08-21 09:34:11 +0700

    Add Person model

---
+++ OfflineFingerprint.Collector/Models/Person.cs
@@ -0,0 +1,12 @@
+namespace OfflineFingerprint.Collector.Models;
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public string PersonCode { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime UpdatedAtUtc { get; set; }
+    public List<FingerprintImage> FingerprintImages { get; set; } = [];
+}
